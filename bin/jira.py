dd6a328f91c357a48e0c8faa29781c8734a6e798
Russell Uman <ruman@splunk.com>
2013-02-25 16:51:17 -0800

    get configuration working. add a logger. give credit to ssorkin. start thinking about making custom fields configurable. stole a lot of stuff from the MySQL app for this.

--- bin/jira.py
+++ bin/jira.py
@@ -1,54 +1,81 @@
-import urllib
-import urllib2
+"""
+External search command for querying a JIRA instance. 
+
+Usage:
+  | jira "JQL query" | ... <other splunk commands here> ...
+
+http://jira.example.com/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=<JQL>&tempMax=count&page/start=offset
+
+Author: Russell Uman
+"""
+
 import base64
-import lxml.etree as et
-import time
 import datetime
+import lxml.etree as et
+import splunk.bundle as sb
+import splunk.mining.dcutils as dcu
 import splunk.Intersplunk as isp
 import sys
+import time
+import urllib
+import urllib2
 
-# http://jira.example.com/sr/jira.issueviews:searchrequest-xml/16483/SearchRequest-16483.xml?tempMax=1000
-# http://jira.example.com/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=project%3DExample&tempMax=1000
+results, dummyresults, settings = isp.getOrganizedResults()
+messages = {}
+logger = dcu.getLogger()
 
-if len(sys.argv)>1:
-    jql = sys.argv[1]
-else:
-    jql = 'project=Example'
-
-keys = ['link', 'project', 'key', 'summary', 'type', 'priority', 'status', 'resolution', 'assignee', 'reporter', 'created', 
-    'updated', 'resolved', 'fixVersion']
-custom_keys = ['Sprint', 'Scrum', 'Story Points','Epic/Theme']
+keys = ['link', 'project', 'key', 'summary', 'type', 'priority', 'status', 'resolution', 'assignee', 'reporter', 'created', 'updated', 'resolved', 'fixVersion']
 times = ['timeestimate', 'timeoriginalestimate', 'timespent']
-
-# TODO: Get this from config
-hostname = 'jira.example.com'
-username = 'admin'
-password = 'changeme'
-count = 100
 offset = 0
 
-while True:
-    query = urllib.urlencode({'jqlQuery':jql, 'tempMax':count, 'pager/start':offset})
-    request = urllib2.Request("https://%s/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?%s" % (hostname, query))
+try:
+   conf_file = 'jira'
+   namespace = 'jira'
+   owner = settings.get("owner", None)
+   sessionKey = settings.get("sessionKey", None)
+   stanza_name = 'jira'
 
-    request.add_header('Authorization', "Basic %s" % base64.b64encode("%s:%s" % (username, password)))
-    result = urllib2.urlopen(request)
+   conf = sb.getConf(conf_file, namespace=namespace, owner=owner, sessionKey=sessionKey)
+   stanza = conf.get(stanza_name)
 
-    root = et.parse(result)
+except Exception, e:
+   logger.error(str(e))
+   isp.addErrorMessage(messages, str(e))
+   isp.outputResuts(results, messages)
 
-    results = []
-    added_count = 0
-    for elem in root.iter('item'):
-        added_count = added_count + 1
-        row = dict(map((lambda k: (k, elem.findtext(k))), keys))
-        
+custom_keys = ['Sprint', 'Scrum', 'Story Points', 'Epic/Theme']
+hostname = stanza.get('hostname')
+username = stanza.get('username')
+password = stanza.get('password')
+count = stanza.get('count')
 
-        for k in times:
+if len(sys.argv) > 1:
+   jql = sys.argv[1]
+else:
+   jql = "project=%s" % stanza.get('default_project')
+
+try:
+   while True:
+      query = urllib.urlencode({'jqlQuery':jql, 'tempMax':count, 'pager/start':offset})
+      request = urllib2.Request("https://%s/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?%s" % (hostname, query))
+
+      request.add_header('Authorization', "Basic %s" % base64.b64encode("%s:%s" % (username, password)))
+      result = urllib2.urlopen(request)
+
+      root = et.parse(result)
+
+      results = []
+      added_count = 0
+      for elem in root.iter('item'):
+         added_count = added_count + 1
+         row = dict(map((lambda k: (k, elem.findtext(k))), keys))
+
+         for k in times:
             v = elem.xpath(k)
             if len(v) == 1:
-                row[k] = v[0].get("seconds")
+               row[k] = v[0].get("seconds")
 
-        for k in custom_keys:
+         for k in custom_keys:
             v = elem.xpath('customfields/customfield/customfieldvalues/customfieldvalue[../../customfieldname/text() = "%s"]' % k)
             if len(v) > 0:
                row[k] = ",".join([val.text for val in v])
@@ -56,13 +83,14 @@ while True:
             v = elem.xpath('customfields/customfield/customfieldvalues/label[../../customfieldname/text() = "%s"]' % k)
             if len(v) > 0:
                row[k] = ",".join([val.text for val in v])
-        results.append(row)
-        #print row
+         results.append(row)
+         #print row
 
-    if added_count > 0:
-        isp.outputResults(results)
-        #print results
-        offset = offset + added_count
+      if added_count > 0:
+         isp.outputResults(results)
+         offset = offset + added_count
 
-    if added_count < count:
-        break
+      if added_count < count:
+         break
+except Exception, e:
+   logger.error(str(e))
